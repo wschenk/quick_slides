d3afe0090fb8d292890aa00f242ac8d4cc400425
Will Schenk <wschenk@gmail.com>
2024-09-12 15:29:54 -0400

    added prev and next and n for new slide

--- compress.js
+++ compress.js
@@ -1,3 +1,4 @@
+// compress.js
 import pako from 'https://cdn.jsdelivr.net/npm/pako@2.1.0/+esm'
 
 export function deflateToBase64( inputData ) {
--- scripts.js
+++ scripts.js
@@ -1,101 +1,103 @@
 import {deflateToBase64, inflateFromBase64} from './compress.js'
 
-    class SlidesHolder extends HTMLElement {
-        constructor() {
-            super();
-            // Treat the whole thing as a template
+class SlidesHolder extends HTMLElement {
+    constructor() {
+        super();
+        // Treat the whole thing as a template
+        
+        this.template = `${this.innerHTML}`;
+        
+        const header = `<header>
+ <button id="saveState">Save State</button>
+ <button id="newSlide">New Slide</button>
+ <button id="nextSlide">Next Slide</button>
+ </header>`;
+
+        // default
+        let sections = [`<section>${this.template}</section>`];
+
+        // load from cache if exists
+        const hash = window.location.hash;
+        if( hash != '' ) {
+            sections = [];
             
-            this.template = `${this.innerHTML}`;
-            
-            const header = `<header>
-  <button id="saveState">Save State</button>
-  <button id="newSlide">New Slide</button>
-  <button id="nextSlide">Next Slide</button>
+            const state = JSON.parse(inflateFromBase64( hash.substring( 1 ) ))
+            for( let section of state ) {
+                let frag = `<section><content-slide>`
 
-</header>`;
-
-            // default
-            let sections = [`<section>${this.template}</section>`];
-
-            // load from cache if exists
-            const hash = window.location.hash;
-            if( hash != '' ) {
-                sections = [];
+                for( let elem of section ) {
+                    frag += `<${elem.nodeName}>${elem.text}</${elem.nodeName}>`
+                }
                 
-                const state = JSON.parse(inflateFromBase64( hash.substring( 1 ) ))
-                for( let section of state ) {
-                    let frag = `<section><content-slide>`
+                frag += "</content-slide></section>"
 
-                    for( let elem of section ) {
-                        frag += `<${elem.nodeName}>${elem.text}</${elem.nodeName}>`
-                    }
-                    
-                    frag += "</content-slide></section>"
-
-                    sections.push( frag );
-                }
+                sections.push( frag );
             }
-                    
-            this.innerHTML = `${header}${sections.join( "\n")}`
+        }
+                
+        this.innerHTML = `${header}${sections.join( "\n")}`
+    }
+    
+    connectedCallback() {
+        this.querySelector( "#nextSlide" ).
+            addEventListener( "click", () => this.nextSlide() );
+        this.querySelector( "#newSlide" ).
+            addEventListener( "click", () => this.newSection() );
+        this.querySelector( "#saveState" ).
+            addEventListener( "click", () => this.getState() );
+    }
+    
+    getState() {
+        const state = []
+        for( let child of this.children ) {
+            const slide = child.querySelector( "content-slide" );
+            if( slide ) {
+                state.push(slide.getState())
+            }
         }
         
-        connectedCallback() {
-            this.querySelector( "#nextSlide" ).addEventListener( "click", () => this.nextSlide() );
-            this.querySelector( "#newSlide" ).addEventListener( "click", () => this.newSection() );
-            this.querySelector( "#saveState" ).addEventListener( "click", () => this.getState() );
-        }
-        
-        getState() {
-            const state = []
-            for( let child of this.children ) {
-                const slide = child.querySelector( "content-slide" );
-                if( slide ) {
-                    state.push(slide.getState())
-                }
-            }
-
-            const json = JSON.stringify(state);
-            window.location.hash = deflateToBase64( json );
-        }
-
-        newSection() {
-            this.innerHTML += `<section>${this.template}</section>`;
-            this.connectedCallback();
-        }
-
-        nextSlide() {
-            let first = true;
-            let lastneg = false
-            for( let section of this.querySelectorAll("section" ) ) {
-                let top = section.getBoundingClientRect().top
-                if( top > 0 && lastneg ) {
-                    section.scrollIntoView()
-                    return;
-                }
-                lastneg = top <= 0
-                if( first && top > 0 ) {
-                    lastneg = true;
-                    }
-                first = false;
-            }
-        }
-
-        prevSlide() {
-            let last = undefined
-            for( let section of this.querySelectorAll("section" ) ) {
-                let top = section.getBoundingClientRect().top
-
-                if( top >= 0 && last) {
-                    last.scrollIntoView();
-                    return;
-                    }
-                last = section;
-            }
-        }
-
+        const json = JSON.stringify(state);
+        window.location.hash = deflateToBase64( json );
     }
 
-    customElements.define("slides-holder", SlidesHolder);
+    newSection() {
+        this.innerHTML += `<section>${this.template}</section>`;
+        this.connectedCallback();
+    }
+
+    nextSlide() {
+        let first = true;
+        let lastneg = false
+        for( let section of this.querySelectorAll("section" ) ) {
+            let top = section.getBoundingClientRect().top
+            if( top > 0 && lastneg ) {
+                section.scrollIntoView()
+                return;
+            }
+            lastneg = top <= 0
+            if( first && top > 0 ) {
+                lastneg = true;
+                }
+            first = false;
+        }
+    }
+
+    prevSlide() {
+        let last = undefined
+        for( let section of this.querySelectorAll("section" ) ) {
+            let top = section.getBoundingClientRect().top
+
+            if( top >= 0 && last) {
+                last.scrollIntoView();
+                return;
+                }
+            last = section;
+        }
+    }
+
+}
+
+customElements.define("slides-holder", SlidesHolder);
 
 document.addEventListener('keydown', function(event) {
     const body = document.querySelector( "body" );
@@ -134,7 +136,7 @@ class ContentSlide extends HTMLElement {
     getState() {
         let state = [];
         for( let child of this.children ) {
-            state.push( {nodeName: child.nodeName, text: child.innerText})
+            state.push( {nodeName: child.nodeName, text: child.innerHTML})
         }
 
         return state;
